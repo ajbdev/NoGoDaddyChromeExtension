b719ffa81ea0dc5a13b141bdcd92b1c7879c7014
Ali Karbassi <ali@karbassi.com>
2011-12-25 13:45:50 -0600

    Rewrote url checker to not loop. Move sopa.js inside bg.html

--- sopa.js
+++
@@ -1,5 +0,0 @@
-if (!document.getElementById('stopSOPA')) {
-   var content = "<div id='stopSOPA' style='background: #cc103f; top: 0; padding: 5px; text-align: center; z-index: 99999; font-size: 15px; line-height: 1.5; width: 100%; color: #fff; font-family: Helvetica, Arial;'><span style='font-weight: bold;'>SOPA Supporter!</span> This company is a known supporter of the dangerous &lsquo;Stop Online Piracy Act.&rsquo;</div>";
-
-   document.body.innerHTML = content + document.body.innerHTML;
-}
