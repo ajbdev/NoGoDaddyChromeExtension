892d9cd6a847212e085ef4dd1ea1485a67fca13d
Tony Webster <tonywebster@gmail.com>
2012-01-04 19:55:10 -0800

    Updating copy to reflect SOPA and PIPA; "Internet Blacklist Legislation" term came from EFF: https://www.eff.org/issues/coica-internet-censorship-and-copyright-bill

--- sopa.js
+++ sopa.js
@@ -1,4 +1,4 @@
 if (!document.getElementById('stopSOPA')) {
-    var content = "<div id='stopSOPA' style='background: #cc103f; top: 0; padding: 5px; text-align: center; z-index: 99999; font-size: 15px; line-height: 1.5; width: 100%; color: #fff; font-family: Helvetica, Arial;'><span style='font-weight: bold;'>SOPA Supporter!</span> This company is a known supporter of the dangerous &lsquo;Stop Online Piracy Act.&rsquo;</div>";
+    var content = "<div id='stopSOPA' style='background: #cc103f; top: 0; padding: 5px; text-align: center; z-index: 99999; font-size: 14.5px; line-height: 1.5; width: 100%; color: #fff; font-family: Helvetica, Arial;'><span style='font-weight: bold;'>Internet Blacklist Legislation Supporter!</span> This company is a known supporter of the dangerous SOPA or PIPA legislation.</div>";
     document.body.innerHTML = content + document.body.innerHTML;
 }
